e2f6b6993bf0107de87df64236f8e3e4b4886e59
Humaid <humaidhusain98@gmail.com>
2022-05-18 22:17:48 +0530

    Making Styled components server side rendered

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,32 @@
 /* eslint-disable @next/next/no-sync-scripts */
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
+import { ServerStyleSheet } from 'styled-components'
+
 
 export default class CustomDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
+  static async getInitialProps(ctx:DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    return initialProps;
+    try {
+    
+      ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          sheet.collectStyles(<App {...props} />),
+      })
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [...React.Children.toArray(initialProps.styles), sheet.getStyleElement()]
+      };
+    } finally {
+      sheet.seal()
+    }
   }
+
   render(): JSX.Element {
     return (
       <Html>
